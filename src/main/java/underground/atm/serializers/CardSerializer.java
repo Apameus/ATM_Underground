b0e3d8fb12636d43f5dca8423271bbf78d57ce3e
AncientGreekProgrammer <Iwannis123@outlook.com.gr>
2024-11-28 12:34:50 +0100

    Card Serializer test added

--- src/main/java/underground/atm/serializers/CardSerializer.java
+++ src/main/java/underground/atm/serializers/CardSerializer.java
@@ -5,7 +5,7 @@ import underground.atm.data.Card;
 public final class CardSerializer {
     public Card parse(String line){
         String[] values = line.split(",");
-        return new Card(Integer.parseInt(values[1]), Integer.parseInt(values[2]), Integer.parseInt(values[3]));
+        return new Card(Integer.parseInt(values[0]), Integer.parseInt(values[1]), Integer.parseInt(values[2]));
     }
 
     public String serialize(Card card){
