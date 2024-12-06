05bd050c2bc568bff425417d4e6601aed54d2b8f
AncientGreekProgrammer <Iwannis123@outlook.com.gr>
2024-12-06 20:51:06 +0100

    RequestCodec refactored

---
+++ src/main/java/underground/atm/common/RequestCodec.java
@@ -0,0 +1,69 @@
+package underground.atm.common;
+
+import underground.atm.common.Request.*;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public final class RequestCodec {
+    public Request decode(DataInputStream inputStream) throws IOException {
+        byte type = inputStream.readByte();
+        return switch (type) {
+            case AuthorizeRequest.TYPE -> {
+                int id = inputStream.readInt();
+                int pin = inputStream.readInt();
+                yield new AuthorizeRequest(id,String.valueOf(pin));
+            }
+            case DepositRequest.TYPE -> {
+                int id = inputStream.readInt();
+                int amount = inputStream.readInt();
+                yield new DepositRequest(id,amount);
+            }
+            case WithdrawRequest.TYPE -> {
+                int id = inputStream.readInt();
+                int amount = inputStream.readInt();
+                yield new WithdrawRequest(id,amount);
+            }
+            case TransferRequest.TYPE -> {
+                int fromId = inputStream.readInt();
+                int toId = inputStream.readInt();
+                int amount = inputStream.readInt();
+                yield new TransferRequest(fromId, toId, amount);
+
+            }
+            default -> throw new IOException("Unknown Request");
+        };
+    }
+
+    public void encode(DataOutputStream outputStream, Request request) throws IOException {
+        switch (request) {
+            case AuthorizeRequest(int id, String pin) -> {
+                outputStream.write(AuthorizeRequest.TYPE);
+                outputStream.writeInt(id);
+                outputStream.writeInt(Integer.parseInt(pin));
+            }
+            case DepositRequest(int id, int amount) -> {
+                outputStream.write(DepositRequest.TYPE);
+                outputStream.writeInt(id);
+                outputStream.writeInt(amount);
+            }
+            case WithdrawRequest(int id , int amount) -> {
+                outputStream.write(WithdrawRequest.TYPE);
+                outputStream.writeInt(id);
+                outputStream.writeInt(amount);
+            }
+            case TransferRequest(int fromId, int toId, int amount) -> {
+                outputStream.write(TransferRequest.TYPE);
+                outputStream.writeInt(fromId);
+                outputStream.writeInt(toId);
+                outputStream.writeInt(amount);
+            }
+            case ViewBalanceRequest(int id) -> {
+                outputStream.write(ViewBalanceRequest.TYPE);
+                outputStream.writeInt(id);
+            }
+        }
+    }
+
+}
